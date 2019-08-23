78f3f7a24a4bfa366e7d1372afe0021dfd7e7d79
Tracie T <traciecodes@gmail.com>
2019-08-23 11:17:31 -0500

    Private Route for bubble page set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,29 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+
+//Build private route that renders BubblePage component
+const PrivRoute = ({ component: Component, ...rest }) => {
+  return <Route {...rest} render={ props => {
+    if (localStorage.getItem("token")) {
+      return <Component {...props} />;
+    } else {
+      return <Redirect to="/login" />;
+    }
+  }} />
+}
+
+const privRoute = Component => props => {
+  if (localStorage.getItem("token")) {
+    return <Component {...props} />;
+  } else {
+    return <Redirect to="/login" />;
+  }
+}
+
+const PrivBubblePage = privRoute(BubblePage);
 
 function App() {
   const [colorList, setColorList] = useState([]);
@@ -10,10 +31,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+      <Route path="/bubblepage" component={PrivBubblePage} />
       </div>
     </Router>
   );
